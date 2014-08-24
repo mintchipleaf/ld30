4bde4b3a1b509b0e44d23033951dcf872e11c19a
mintleaf <mintchipleaf@gmail.com>
2014-08-23 20:52:36 -0400

    Add basic train animation

---
+++ Assets/Scripts/TrainAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainAnimation : MonoBehaviour {
+
+	public GameObject train;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		train.animation.Play();
+	}
+}
