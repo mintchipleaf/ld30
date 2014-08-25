f1e4bd6fad56d81249f864ccb07e4025c12963a3
mintleaf <mintchipleaf@gmail.com>
2014-08-24 21:05:22 -0400

    Final LD30 commit

--- Assets/Scripts/TrainArrive.cs
+++ Assets/Scripts/TrainArrive.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class TrainArrive : MonoBehaviour {
 
 	public GameObject train;
+	public GameObject block;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	void OnTriggerEnter(Collider other) {
+	IEnumerator Start () {
+		yield return new WaitForSeconds(60);
 		train.animation.Play();
-		Debug.Log("Ass");
+		Object.Destroy(block);
 	}
+
+
+
 }
